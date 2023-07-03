ace615041206df4f2821225bce94eee97b0acf76
mossa <martin.ossa.8@gmail.com>
2023-07-03 12:58:55 +0200

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'password',
+    confirmation: 'verification',
+    unlock: 'unlock',
+    sign_up: 'signup',
+    edit: 'edit'
+  }, controllers: { registrations: 'registrations'}
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
